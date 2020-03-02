52bcbf802267ba2e7059961690514e7a129aa2cc
Paul Halloran <paul.halloran@gmail.com>
2020-03-02 21:35:03 +0000

    update

--- plot_east_west_depth_section.py
+++ plot_east_west_depth_section.py
@@ -5,17 +5,22 @@ import iris
 
 def plot(lon_west,lon_east,lat_south,lat_north,cube):
     cube_region_tmp = cube.intersection(longitude=(lon_west, lon_east))
-    cube = cube_region_tmp.intersection(latitude=(lat_south, lat_north))
+    cube_cube_region = cube_region_tmp.intersection(latitude=(lat_south, lat_north))
     try:
-        cube.coord('latitude').guess_bounds()
+        cube_cube_region.coord('latitude').guess_bounds()
     except:
         pass
     try:
-        cube.coord('longitude').guess_bounds()
+        cube_cube_region.coord('longitude').guess_bounds()
     except:
         pass
-    grid_areas = iris.analysis.cartography.area_weights(cube)
-    section_avg_cube = cube.collapsed(['latitude'], iris.analysis.MEAN, weights=grid_areas)
-    plt.figure(figsize=(10,12))
+    grid_areas = iris.analysis.cartography.area_weights(cube_cube_region)
+    section_avg_cube = cube_cube_region.collapsed(['latitude'], iris.analysis.MEAN, weights=grid_areas)
+    plt.figure(figsize=(12, 5))
+    plt.subplot(121)
+    qplt.contourf(cube[0],20)
+    plt.plot([lon_west,lon_east],[((lat_south-lat_north)/2.0),((lat_south-lat_north)/2.0)])
+    plt.gca().coastlines()
+    plt.subplot(122)
     qplt.contourf(section_avg_cube,20)
     plt.show()
--- plot_north_south_depth_section.py
+++ plot_north_south_depth_section.py
@@ -6,17 +6,22 @@ import iris
 
 def plot(lon_west,lon_east,lat_south,lat_north,cube):
     cube_region_tmp = cube.intersection(longitude=(lon_west, lon_east))
-    cube = cube_region_tmp.intersection(latitude=(lat_south, lat_north))
+    cube_region = cube_region_tmp.intersection(latitude=(lat_south, lat_north))
     try:
-        cube.coord('latitude').guess_bounds()
+        cube_region.coord('latitude').guess_bounds()
     except:
         pass
     try:
-        cube.coord('longitude').guess_bounds()
+        cube_region.coord('longitude').guess_bounds()
     except:
         pass
-    grid_areas = iris.analysis.cartography.area_weights(cube)
-    section_avg_cube = cube.collapsed(['longitude'], iris.analysis.MEAN, weights=grid_areas)
-    plt.figure(figsize=(10,12))
+    grid_areas = iris.analysis.cartography.area_weights(cube_region)
+    section_avg_cube = cube_region.collapsed(['longitude'], iris.analysis.MEAN, weights=grid_areas)
+    plt.figure(figsize=(12, 5))
+    plt.subplot(121)
+    qplt.contourf(cube[0],20)
+    plt.plot([((lon_east-lon_west)/2.0)+lon_west,((lon_east-lon_west)/2.0)+lon_west],[lat_south,lat_north])
+    plt.gca().coastlines()
+    plt.subplot(122)
     qplt.contourf(section_avg_cube,20)
     plt.show()
